423e7d1a185771d8d6d02a59b96ccaa6aaafc6a5
D'Attoma <bertodattoma@gmail.com>
2019-11-18 16:41:34 +0100

    class customer host designer added

--- AnomalyDetector/anomaly_detector.py
+++ AnomalyDetector/anomaly_detector.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import AnomalyDetector.data_manager as data_manager
 import AnomalyDetector.ground_truth_generator as gtg
+import AnomalyDetector.approx_ground_truth_generator as agtg
 import AnomalyDetector.Tools.sample_extractor as sample_extractor
 from AnomalyDetector.Tools.sample_clustering import Modeler
 
@@ -26,7 +27,7 @@ class CustomerHostTrainer():
         self.guardperiod = ''
         self.json_path = json_path
         self.groundtruths_dict = {}
-        self.gtg_instance = None
+        #self.gtg_instance = None
 
         self.import_data()
         
@@ -96,7 +97,7 @@ class CustomerHostTrainer():
 
 class CustomerHostDesigner():
 
-    def __init__(self,n_interactions=10, clustering_algo_name='kmeans', customer='' , network='', source='', db='',\
+    def __init__(self, n_interactions=15, clustering_algo_name='kmeans', customer='' , network='', source='', db='',\
          host='', labeler='', labeling_model='', experiment_start='', \
          experiment_end='', time_zone='', n_ago='', event_period='', guardperiod=''):
 
@@ -120,10 +121,11 @@ class CustomerHostDesigner():
         self.groundtruths_dict = {}
         self.model = None
         self.df_samples_cluster = None
+        self.df_samples_reduce = None
 
-        self.import_data_designer()
+        self.import_data()
     
-    def import_data_designer(self):
+    def import_data(self):
         """import via data_manager the events dataframe"""
 
         experiment_start = pd.to_datetime('today',format='%Y-%m-%d %H:%M:%S')
@@ -158,17 +160,81 @@ class CustomerHostDesigner():
 
         print('---Importing data via data manager: DONE')
         
-    def get_samples(self):
+    def get_samples(self,reduction=False, n_components=2):
         extractor = sample_extractor.SampleExtractor(self.list_events)
         extractor.generate_samples(feature_engineering=True)
         self.df_samples = extractor.df_samples.copy()
+        if reduction:
+            extractor.apply_pca(n_components)
+            self.df_samples_reduce = extractor.df_samples_reduce.copy()
 
-    def get_clustering_model(self, algorithm_name, elbow=False):
-        clusterizer = Modeler(self.df_samples,self.n_interactions)
+    def get_clustering_model(self, algorithm_name='kmeans', elbow=False, print_clusters=False):
+        if not self.df_samples_reduce.empty:
+            clusterizer = Modeler(self.df_samples_reduce,self.n_interactions)
+        else:
+            clusterizer = Modeler(self.df_samples,self.n_interactions)
         if algorithm_name.lower().strip() == 'kmeans':
             if elbow == True:
-                clusterizer.model_kmeans(elbow=True)
+                clusterizer.model_kmeans(elbow=True, print_clusters=print_clusters)
             else:
-                self.model,self.df_samples_cluster = clusterizer.model_kmeans(elbow=False,ncluster=self.n_interactions)
+                self.model,self.df_samples_cluster = clusterizer.model_kmeans(elbow=False,\
+                                                ncluster=self.n_interactions, print_clusters=print_clusters)
 
 
+    def instantiate_approx_gtg(self):
+        """ if not self.df_samples_reduce.empty:
+            agtg_instance = agtg.ApproxGroudTruthGenerator(clustering_model=self.model, \
+                            df_samples=self.df_samples_reduce, n_interactions=self.n_interactions)
+        else:
+            agtg_instance = agtg.ApproxGroudTruthGenerator(clustering_model=self.model, \
+                            df_samples=self.df_samples, n_interactions=self.n_interactions) """
+        agtg_instance = agtg.ApproxGroudTruthGenerator(clustering_model=self.model, \
+                            df_samples_cluster=self.df_samples_cluster, n_interactions=self.n_interactions)
+        return agtg_instance
+
+
+    def generate_ground_truth(self):
+
+        experiment_start = pd.to_datetime('today',format='%Y-%m-%d %H:%M:%S')
+        experiment_start = pd.to_datetime(str(experiment_start).split('.')[0])
+        if self.n_ago != '' and (not self.experiment_start and not self.experiment_end) :
+            delta = [s for s in self.n_ago if s.isdigit()]
+            delta = ''.join(delta)
+            if 'day' in self.n_ago:
+                experiment_end = experiment_start - pd.to_timedelta(int(delta),'days')
+            elif 'hour' in self.n_ago:
+                experiment_end = experiment_start - pd.to_timedelta(int(delta),'hours')
+        else:
+            experiment_start = self.experiment_start 
+            experiment_end = self.experiment_end
+
+        if self.event_period == '':
+            self.event_period = '15m'
+        if not self.df_samples_reduce.empty:
+            df_event_index = pd.DatetimeIndex(self.df_samples_reduce.index)
+        else:
+            df_event_index = pd.DatetimeIndex(self.df_samples.index)
+
+        gtg_instance = gtg.GroudTruthGenerator(self.labeler, self.labeling_model, experiment_start, experiment_end, \
+                                        self.event_period, self.guardperiod, self.time_zone, df_event_index, \
+                                        self.groundtruths_dict)
+        
+        for serie,data in gtg_instance.class_dict.items():
+            if not serie == 'default_class_value' and not serie == 'invalid_class_value' and not serie == 'period' and not data['type'] == 'default':
+                print('Periods of class',serie,' manually labeled by a human labeler and reported in json file')
+                print(data['periods'])
+                print('\n')
+                #generate signals for each class
+
+        gtg_instance.get_class_continuous_signal()
+        
+        #generate the ground truth signal - gtg_instance.gt_series attribute
+        gtg_instance.get_ground_truth_continuous_signal()
+
+        #perform event labeling (v1 - using inclusion criterion) - gtg_instance.series_events_label
+        gtg_instance.perform_event_labeling()
+
+        #perform event labeling (v2 - using gt signal event extraction) - gtg_instance.gt_series_events_label
+        gtg_instance.perform_event_labeling_from_gt_signal()
+
+        return gtg_instance
