1dce2da9b59fb8d9c76e8fbcab2c8f00f961530f
D'Attoma <bertodattoma@gmail.com>
2019-11-18 16:39:01 +0100

    fix index df clusters

--- AnomalyDetector/Tools/sample_clustering.py
+++ AnomalyDetector/Tools/sample_clustering.py
@@ -9,16 +9,17 @@ class Modeler():
         self.df_samples = df_samples
         self.n_interactions = n_interactions
 
-    def model_kmeans(self, elbow=False, ncluster=10):
-        
+    def model_kmeans(self, elbow=False, ncluster=15, print_clusters=False):
+        X = self.df_samples
+            
         if elbow == True:
             n_cluster = range(1, 20)
             distorsions = []
             for i in n_cluster:
                 print('\nCluster',i)
-                km = KMeans(n_clusters=i).fit(self.df_samples)
+                km = KMeans(n_clusters=i).fit(X)
                 distorsions.append(km.inertia_)
-                print('\nCluster',i,' finito')
+                print('\nCluster',i,' finito, - inertia: ',km.inertia_)
             
             fig, ax = plt.subplots(figsize=(10,6))
             ax.plot(n_cluster, distorsions, 'bx-')
@@ -26,21 +27,32 @@ class Modeler():
             plt.ylabel('Sum of squared distances')
             plt.title('Elbow Method For Optimal k')
             plt.show() 
+
+            if print_clusters:#print 20 clusters
+                plt.scatter(X[:,0],X[:,1], c=km.labels_, cmap='rainbow')
+                plt.scatter(km.cluster_centers_[:,0] ,km.cluster_centers_[:,1], color='black',marker="*")
+                plt.show()
+
             return None
         
         else:
             print('\nCluster number:',ncluster)
-            km = KMeans(n_clusters=ncluster,max_iter=500).fit(self.df_samples)
-            km.fit(self.df_samples)
-            km.predict(self.df_samples)
+            km = KMeans(n_clusters=ncluster,max_iter=600).fit(X)
+            km.fit(X)
+            km.predict(X)
             distorsion = km.inertia_
             print('\nCluster',ncluster,' finito - distorsion:',distorsion)
             print(km.labels_)
 
-            df_temp = self.df_samples.copy()
+            #df_temp = self.df_samples.copy()
+            df_temp = X.copy()
             df_temp = df_temp.reset_index()
             for i in df_temp.index:
                 df_temp.loc[i,'cluster'] = km.labels_[i]
             df_temp.loc[:,['index','cluster']].to_csv('kmeans_clusters_event.csv',sep=';')
-
+            if print_clusters:
+                plt.scatter(X.iloc[:,0],X.iloc[:,1], c=km.labels_, cmap='rainbow')
+                plt.scatter(km.cluster_centers_[:,0] ,km.cluster_centers_[:,1], color='black',marker="*")
+                plt.show()
+            df_temp = df_temp.set_index('index')
             return km,df_temp
